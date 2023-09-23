2bf633d17e8b07c97b10049f9c10e70999f45c0b
fepec <felipe.estrada.prada@gmail.com>
2023-09-22 19:52:35 -0500

    Revert order by PokeID changes

--- js/pokemon.js
+++ js/pokemon.js
@@ -3,21 +3,15 @@ const API_POKEMON = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0";
 
 // funciones
 
-function comparePokeId(a,b) {
-// utilizada para en un sort para ordenar Pokémones en orden de id.
-  if (a.id < b.id) {
-    return -1;
-  }
-  if (a.id > b.id) {
-    return 1;
-  }
-  return 0;
-}
+// function comparePokeId(a, b) {
+//   // utilizada para en un sort para ordenar Pokémones en orden de id.
+//   return a.id - b.id;
+// } 
 
 function getPokemon(api) {
-  document.getElementById("dataAlbum").innerHTML = ""
+  document.getElementById("dataAlbum").innerHTML = "";
   fetch(api)
-    .then((response) => response.json())  // .json() convierte la respuesta en un array de JS objects
+    .then((response) => response.json()) // .json() convierte la respuesta en un array de JS objects
     .then((json) => {
       getPokemonData(json.results);
       pagination(json);
@@ -28,29 +22,28 @@ function getPokemon(api) {
 }
 
 function fillCards(json) {
-  html = `<div class="col col-6 col-sm-4 col-md-2 card">
-              <div class="card-body">
+    
+    html = `<div class="col col-6 col-sm-4 col-md-2 card ">
+              <div class="card-body p-0 bg-secondary-subtle">
                 <img class="card-img-top" src="${json.sprites.other["official-artwork"].front_default}" alt="A picture of ${json.name}.">
-                <p class="card-title fs-6 fw-normal ">Number: ${json.id}</p>  
-                <p class="card-title fs-6 fw-normal ">Name: ${json.name}</p>
+                <p class="card-title fs-6 fw-normal bg-body">Number: ${json.id}</p>  
+                <p class="card-title fs-6 fw-normal bg-body">Name: ${json.name}</p>
               </div>
-            </div>`;
+            </div>`;    
+   
   document.getElementById("dataAlbum").innerHTML += html;
 }
 
 function getPokemonData(poke) {  
-  console.log(poke)
-  poke.sort(comparePokeId);
+  let pokemons = [];
   for (let i = 0; i < poke.length; i++) {
     fetch(poke[i].url)
       .then((response) => response.json())
-      .then((json) => {
-        fillCards(json);
-      })
+      .then((json) => fillCards(json))
       .catch((error) => {
         console.log("Error obteniendo imagen:", error);
       });
-  }
+  }  
 }
 
 function pagination(info) {
